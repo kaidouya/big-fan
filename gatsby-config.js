3f5a42e80f4e7a95349fccd40a771e815fcd5a72
kaidouya <kaidouya0527@gmail.com>
2020-03-18 01:07:44 +0800

    Add plug

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,9 +28,9 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-no-sourcemaps",
+    },
   ],
   pathPrefix: '/big-fan',
 };
